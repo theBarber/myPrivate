7427700ccd204a8e164c7240864b1cbc6142ae7a
Aharon Hacmon <aharon.hacmon@perion.com>
2016-09-21 17:20:05 +0300

    nicer

--- src/test/java/com/undertone/ramp/lift/adselector/automation/TestSanity.java
+++ src/test/java/com/undertone/ramp/lift/adselector/automation/TestSanity.java
@@ -1,11 +1,8 @@
 package com.undertone.ramp.lift.adselector.automation;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Description;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,16 +11,13 @@ import cucumber.api.java8.En;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:sanity.feature", plugin = { "pretty", "html:target/cucumber" }
-
-)
+@CucumberOptions(features = "classpath:sanity.feature", plugin = { "pretty", "json:target/cucumber" })
 public class TestSanity implements En {
 
     public TestSanity() {
 	Given("I have (\\d+) cukes in my belly", (Integer cukes) -> {
-	    Assert.assertThat("too much", cukes, CoreMatchers.is(30));
+	    assertThat("too much cucumbers", cukes, is(30));
 	});
-
     }
 
     @Test
