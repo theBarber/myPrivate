9f00a6df1e9ea890e85a83eb5b75e81328e81126
itaypinhassi <itay.pinhassi@perion.com>
2016-09-28 14:12:03 +0300

    mysql tests start

---
+++ src/test/java/com/undertone/ramp/lift/adselector/automation/cache_process.java
@@ -0,0 +1,18 @@
+import cucumber.api.PendingException;
+import cucumber.api.java8.En;
+
+/**
+ * Created by Itay.Pinhassi on 9/28/2016.
+ */
+public class cache_process implements En {
+
+    public cache_process() {
+        Given("^limitations for zoneId (\\d+) is Firefox in Workflow DB$", (Integer arg0) -> {
+            
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+    }
+
+
+}
