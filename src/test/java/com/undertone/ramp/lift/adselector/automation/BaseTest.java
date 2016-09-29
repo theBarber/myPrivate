6a97164ee985a0c6730661eee860e171f419909f
Aharon Hacmon <aharon.hacmon@perion.com>
2016-09-29 18:11:59 +0300

    create cli connections

--- src/test/java/com/undertone/ramp/lift/adselector/automation/BaseTest.java
+++ src/test/java/com/undertone/ramp/lift/adselector/automation/BaseTest.java
@@ -1,8 +1,11 @@
 package com.undertone.ramp.lift.adselector.automation;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
@@ -10,9 +13,18 @@ import java.util.Properties;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 
+import com.undertone.automation.cli.conn.CliConnection;
+import com.undertone.automation.cli.conn.CliConnectionImpl;
+import com.undertone.automation.cli.conn.CliConnectionImpl.EnumConnectionType;
+import com.undertone.automation.cli.conn.LinuxDefaultCliConnection;
+import com.undertone.automation.cli.conn.RootLinuxCliConnection;
+import com.undertone.automation.support.StringUtils;
+
 import cucumber.api.java8.En;
 import cucumber.api.java8.GlueBase;
 import cucumber.api.junit.Cucumber;
+import gherkin.deps.com.google.gson.JsonArray;
+import gherkin.deps.com.google.gson.JsonParser;
 
 @RunWith(Cucumber.class)
 public
@@ -21,13 +33,21 @@ class BaseTest implements En, GlueBase {
 
     protected final String environmentName;
     protected final Map<String, String> config = Collections.synchronizedMap(new HashMap<>());
+    protected final List<CliConnectionImpl> uasCliConnections = new ArrayList<>();
 
+    // protected final Reporter reporter;
     // protected final List<UAS> uas_instances;
     public BaseTest() {
 	environmentName = Optional.ofNullable(System.getenv("ENVIRONMENT")).orElse("ci").toLowerCase();
 	String environmentNameConfigPrefix = environmentName + ".";
-	Before(scenario -> {
+	After(scenario -> {
 	    config.clear();
+	    uasCliConnections.forEach(CliConnection::disconnect);
+	    uasCliConnections.clear();
+	});
+
+	Before(scenario -> {
+
 	    Properties properties = new Properties();
 	    try {
 		properties.load(this.getClass().getClassLoader().getResourceAsStream("environments"));
@@ -40,8 +60,41 @@ class BaseTest implements En, GlueBase {
 		    config.put(configurationKey.substring(environmentNameConfigPrefix.length()), value);
 		}
 	    });
+	    boolean isUasTest = scenario.getSourceTagNames().stream().map(String::toLowerCase)
+		    .anyMatch(tag -> tag.equals("@uas"));
+	    if (isUasTest) {
+		String uasCliConnectionUser = config.get("uas.cliconnection.user");
+		String uasCliConnectionPassword = config.getOrDefault("uas.cliconnection.password", null);
+		String cliConnectionsHostsParam = config.get("uas.cliconnection.hosts");
+		String cliconnectionKeyname = config.getOrDefault("uas.cliconnection.keyname", "");
+		JsonArray hostsConfig = new JsonParser().parse(cliConnectionsHostsParam).getAsJsonArray();
+		File keyFile = Optional.of(cliconnectionKeyname).filter(StringUtils.nonEmpty)
+			.map(filename -> new File(new File(System.getProperty("user.home"), ".ssh"), filename))
+			.filter(File::exists).filter(File::canRead).orElse(null);
 
-	    // scenario.write(scenario.getName() + ": configuration loaded");
+		hostsConfig.forEach(jsonElement -> {
+		    String host = jsonElement.getAsString();
+		    CliConnectionImpl conn;
+
+		    if (uasCliConnectionUser.equals("root")) {
+			conn = new RootLinuxCliConnection();
+		    } else {
+			conn = new LinuxDefaultCliConnection();
+		    }
+
+		    conn.setHost(host);
+		    conn.setUser(uasCliConnectionUser);
+		    conn.setPassword(uasCliConnectionPassword);
+		    conn.setConnectOnInit(true);
+
+		    if (keyFile != null) {
+			conn.setPrivateKey(keyFile);
+			conn.setProtocol(EnumConnectionType.SSH_RSA.value());
+		    }
+
+		    uasCliConnections.add(conn);
+		});
+	    }
 	});
 
     }
--- src/test/java/com/undertone/ramp/lift/adselector/automation/S3LoaderTest.java
+++ src/test/java/com/undertone/ramp/lift/adselector/automation/S3LoaderTest.java
@@ -6,16 +6,20 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.undertone.automation.assertion.Assert;
+import com.undertone.automation.cli.conn.CliCommand;
+import com.undertone.automation.cli.conn.CliConnection;
+import com.undertone.automation.cli.conn.CliConnectionImpl;
 import com.utils.S3Client;
 import com.utils.SshAgent;
 import org.apache.commons.lang.StringEscapeUtils;
-import org.junit.Assert;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by nive on 2016-09-22.
@@ -26,6 +30,7 @@ public class S3LoaderTest extends BaseTest {
 
     public S3LoaderTest() {
 	super();
+	
 	Given("Loading new (\\S+\\.json) to (\\S+\\.json) s3", (String from, String to) -> {
 	    PutObjectResult res = S3Client.getInstance().uploadFile(from, to);
 	    assertNotNull(res);
@@ -39,23 +44,29 @@ public class S3LoaderTest extends BaseTest {
 	    }
 
 	});
-	Then("The ad selector new plan should be (\\S+\\.json)", (String efn) -> {
-	    
-	    
-	    
+	Then("The ad selector new plan should be (\\S+\\.json)", (String json) -> {
 	    SshAgent sshAgent = new SshAgent("src\\test\\resources\\user_info.properties");
-        String command1 = StringEscapeUtils.
-                escapeJava("cat /var/log/ramp-lift-ad-selector/ad-selector.log.1 /var/log/ramp-lift-ad-selector/ad-selector.log > temp_logs");
-        String command2 = StringEscapeUtils.escapeJava("tac temp_logs | egrep  -m1 Loaded | egrep -o  solver_plan_[_[:alnum:]]+\\.json > res_log.txt");
-	    sshAgent.enterSshCommand(command1, 2);
-	    sshAgent.enterSshCommand(command2, 2);
-	    File f = sshAgent.copyFileFromRemote("res_log.txt", 2);
-	    sshAgent.enterSshCommand("rm temp_logs", 1);
-	    
-	    sshAgent.enterSshCommand("rm res_log.txt", 1);
-	    boolean res = sshAgent.compareContent(f, efn);
-	    sshAgent.close();
-	    Assert.assertTrue(res);
+	    String command1 = StringEscapeUtils.escapeJava(
+		    "cat /var/log/ramp-lift-ad-selector/ad-selector.log.1 /var/log/ramp-lift-ad-selector/ad-selector.log > temp_logs");
+	    String command2 = StringEscapeUtils.escapeJava(
+		    "tac temp_logs | egrep  -m1 Loaded | egrep -o  solver_plan_[_[:alnum:]]+\\.json > res_log.txt");
+
+	    CliConnectionImpl cliConnection = uasCliConnections.get(0);
+	    try {
+		cliConnection.init();
+		cliConnection.connect();
+		cliConnection.handleCliCommand("command1", new CliCommand(command1));
+		cliConnection.handleCliCommand("command2", new CliCommand(command2));
+		File f = sshAgent.copyFileFromRemote("res_log.txt", 2);
+		sshAgent.enterSshCommand("rm temp_logs", 1);
+
+		sshAgent.enterSshCommand("rm res_log.txt", 1);
+		boolean res = sshAgent.compareContent(f, json);
+		sshAgent.close();
+		Assert.assertTrue(res);
+	    } catch (InterruptedException | IOException e) {
+		Assert.fail("test failed", e);
+	    }
 	});
     }
 }
