2df99e01465c666a799d0828fd3cbdae53a1c73e
Aharon Hacmon <aharon.hacmon@perion.com>
2016-10-05 13:16:19 +0300

    fix rabbitmq tests

--- src/test/java/com/undertone/ramp/lift/adselector/automation/BaseTest.java
+++ src/test/java/com/undertone/ramp/lift/adselector/automation/BaseTest.java
@@ -7,16 +7,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 
+import com.rabbitmq.client.ConnectionFactory;
 import com.undertone.automation.cli.conn.CliConnection;
 import com.undertone.automation.cli.conn.CliConnectionImpl;
 import com.undertone.automation.cli.conn.CliConnectionImpl.EnumConnectionType;
 import com.undertone.automation.cli.conn.LinuxDefaultCliConnection;
 import com.undertone.automation.cli.conn.RootLinuxCliConnection;
 import com.undertone.automation.support.StringUtils;
+import com.undertone.automation.utils.MsgProcess;
+import com.undertone.automation.utils.RabbitMQConsumer;
+import com.undertone.automation.utils.RabbitMQPublisher;
 
 import cucumber.api.java8.En;
 import cucumber.api.java8.GlueBase;
@@ -25,21 +30,31 @@ import gherkin.deps.com.google.gson.JsonArray;
 import gherkin.deps.com.google.gson.JsonParser;
 
 @RunWith(Cucumber.class)
-public
-
-class BaseTest implements En, GlueBase {
+public class BaseTest implements En, GlueBase {
 
     protected final String environmentName;
     protected final Map<String, String> config = Collections.synchronizedMap(new HashMap<>());
     protected final Map<String, CliConnectionImpl> uasCliConnections = new HashMap<>();
 
+    protected ConnectionFactory connectionFactory = null;
+    protected com.rabbitmq.client.Connection rabbitClientConnection;
+
+    protected RabbitMQPublisher publisher;
+    protected RabbitMQConsumer consumer;
     // protected final Reporter reporter;
     // protected final List<UAS> uas_instances;
+
+    protected final String[] CLITESTS = new String[] { "@cli" };
+    protected final String[] RABBITTESTS = new String[] { "@rabbitmq" };
+
     public BaseTest() {
 	environmentName = Optional.ofNullable(System.getenv("ENVIRONMENT")).orElse("staging").toLowerCase();
 	String environmentNameConfigPrefix = environmentName + ".";
 	After(scenario -> {
 	    config.clear();
+	});
+
+	After(CLITESTS, scenario -> {
 	    uasCliConnections.values().forEach(CliConnection::disconnect);
 	    uasCliConnections.clear();
 	});
@@ -57,42 +72,70 @@ class BaseTest implements En, GlueBase {
 		    config.put(configurationKey.substring(environmentNameConfigPrefix.length()), value);
 		}
 	    });
-	    boolean isCliTest = scenario.getSourceTagNames().stream().map(String::toLowerCase)
-		    .anyMatch(tag -> tag.equals("@cli"));
-	    if (isCliTest) {
-		String uasCliConnectionUser = config.get("uas.cliconnection.user");
-		String uasCliConnectionPassword = config.getOrDefault("uas.cliconnection.password", null);
-		String cliConnectionsHostsParam = config.get("uas.cliconnection.hosts");
-		String cliconnectionKeyname = config.getOrDefault("uas.cliconnection.keyname", "");
-		JsonArray hostsConfig = new JsonParser().parse(cliConnectionsHostsParam).getAsJsonArray();
-		File keyFile = Optional.of(cliconnectionKeyname).filter(StringUtils.nonEmpty)
-			.map(filename -> new File(new File(System.getProperty("user.home"), ".ssh"), filename))
-			.filter(File::exists).filter(File::canRead).orElse(null);
 
-		hostsConfig.forEach(jsonElement -> {
-		    String host = jsonElement.getAsString();
-		    CliConnectionImpl conn;
-
-		    if (uasCliConnectionUser.equals("root")) {
-			conn = new RootLinuxCliConnection();
-		    } else {
-			conn = new LinuxDefaultCliConnection();
-		    }
-
-		    conn.setHost(host);
-		    conn.setUser(uasCliConnectionUser);
-		    conn.setPassword(uasCliConnectionPassword);
-		    conn.setConnectOnInit(true);
-
-		    if (keyFile != null) {
-			conn.setPrivateKey(keyFile);
-			conn.setProtocol(EnumConnectionType.SSH_RSA.value());
-		    }
-		    uasCliConnections.put(host, conn);
-		});
-	    }
 	});
 
+	Before(CLITESTS, scenario -> {
+	    String uasCliConnectionUser = config.get("uas.cliconnection.user");
+	    String uasCliConnectionPassword = config.getOrDefault("uas.cliconnection.password", null);
+	    String cliConnectionsHostsParam = config.get("uas.cliconnection.hosts");
+	    String cliconnectionKeyname = config.getOrDefault("uas.cliconnection.keyname", "");
+	    JsonArray hostsConfig = new JsonParser().parse(cliConnectionsHostsParam).getAsJsonArray();
+	    File keyFile = Optional.of(cliconnectionKeyname).filter(StringUtils.nonEmpty)
+		    .map(filename -> new File(new File(System.getProperty("user.home"), ".ssh"), filename))
+		    .filter(File::exists).filter(File::canRead).orElse(null);
+
+	    hostsConfig.forEach(jsonElement -> {
+		String host = jsonElement.getAsString();
+		CliConnectionImpl conn;
+
+		if (uasCliConnectionUser.equals("root")) {
+		    conn = new RootLinuxCliConnection();
+		} else {
+		    conn = new LinuxDefaultCliConnection();
+		}
+
+		conn.setHost(host);
+		conn.setUser(uasCliConnectionUser);
+		conn.setPassword(uasCliConnectionPassword);
+		conn.setConnectOnInit(true);
+
+		if (keyFile != null) {
+		    conn.setPrivateKey(keyFile);
+		    conn.setProtocol(EnumConnectionType.SSH_RSA.value());
+		}
+		uasCliConnections.put(host, conn);
+	    });
+
+	});
+
+	Before(RABBITTESTS, scenario -> {
+
+	    // the consumer will wait on this queue
+
+	    // will init all the values from the config map object
+	    connectionFactory = new ConnectionFactory();
+	    connectionFactory.setUsername(config.get("de.rabbitmq.user"));
+	    connectionFactory.setPassword(config.get("de.rabbitmq.password"));
+	    connectionFactory.setHost(config.get("de.rabbitmq.host"));
+	    connectionFactory.setPort(Integer.parseInt(config.get("de.rabbitmq.port")));
+
+	    try {
+		rabbitClientConnection = connectionFactory.newConnection();
+		// consumer_tag is basically thread num
+		publisher = new RabbitMQPublisher("in", rabbitClientConnection.createChannel());
+		if (this instanceof MsgProcess) {
+		    MsgProcess thisAsMsgProcess = (MsgProcess) this;
+		    consumer = new RabbitMQConsumer(thisAsMsgProcess, rabbitClientConnection.createChannel(), "test",
+			    "out", "integration-test", "integration-test");
+		    consumer.start();
+		}
+	    } catch (IOException | TimeoutException e) {
+		com.undertone.automation.assertion.Assert.fail("unable to initialize rabbitmq connection", e);
+		e.printStackTrace();
+	    }
+
+	});
     }
 
 }
--- src/test/java/com/undertone/ramp/lift/adselector/automation/RabbitMQClientTest.java
+++ src/test/java/com/undertone/ramp/lift/adselector/automation/RabbitMQClientTest.java
@@ -1,20 +1,17 @@
 package com.undertone.ramp.lift.adselector.automation;
 
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.undertone.automation.utils.MsgProcess;
-import com.undertone.automation.utils.RabbitMQConsumer;
-import com.undertone.automation.utils.RabbitMQPublisher;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.io.IOException;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.TimeoutException;
+
+import com.undertone.automation.utils.MsgProcess;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 
 /**
  * Created by nive on 2016-09-28.
@@ -23,75 +20,34 @@ import java.util.concurrent.TimeoutException;
 @CucumberOptions(features = "classpath:RabbitMQ_integration.feature", plugin = { "pretty", "json:target/cucumber/s3" })
 public class RabbitMQClientTest extends BaseTest implements MsgProcess {
     private static Logger logger = LoggerFactory.getLogger(RabbitMQClientTest.class);
-    protected Connection conn;
-    protected RabbitMQPublisher publisher;
-    protected RabbitMQConsumer consumer;
     private BlockingDeque<String> returnQueue;
     private String messageReply;
 
     public RabbitMQClientTest() {
-        super();
-        Before(scenario->init());
-//        init();
-        When("When putting a (\\S+) on RMQ with banner ID BI  which satisfy criteria A", (String req) -> {
-            String msg = "{\"metadata\":{\"callback_queue\":\"integration-test\",\"response_timeout\":10,\"server_name\":\"server1\",\"context_id\":\"abcd\"},\"body\":{\"features\":{\"zone_id\":1111111,\"brand_name\":\"\",\"mobile_browser\":\"\",\"user_agent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393\",\"region\":\"nc\",\"city\":\"durham\",\"zip\":\"27707\",\"area_codes\":\"919/984\",\"gmt_offset\":\"-400\",\"latitude\":35,\"longitude\":-78,\"device\":\"\",\"os\":\"\",\"dma\":\"\",\"country\":\"us\"},\"eligible_ads\":[{\"id\":\"111111\"}]}}";
-            publisher.put(msg, "in1");
-        });
+	super();
+	returnQueue = new LinkedBlockingDeque<>();
+	When("When putting a (\\S+) on RMQ with banner ID BI  which satisfy criteria A", (String req) -> {
+	    String msg = "{\"metadata\":{\"callback_queue\":\"integration-test\",\"response_timeout\":10,\"server_name\":\"server1\",\"context_id\":\"abcd\"},\"body\":{\"features\":{\"zone_id\":1111111,\"brand_name\":\"\",\"mobile_browser\":\"\",\"user_agent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393\",\"region\":\"nc\",\"city\":\"durham\",\"zip\":\"27707\",\"area_codes\":\"919/984\",\"gmt_offset\":\"-400\",\"latitude\":35,\"longitude\":-78,\"device\":\"\",\"os\":\"\",\"dma\":\"\",\"country\":\"us\"},\"eligible_ads\":[{\"id\":\"111111\"}]}}";
+	    publisher.put(msg, "in1");
+	});
 
-        Then("The RMQ should return reply with  banner id (\\d+)", (String id) -> {
-            try {
-                messageReply = returnQueue.take();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Assert.assertTrue(messageReply.contains(id));
-            try {
-                consumer.close();
-                conn.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-
-            }
-        });
+	Then("The RMQ should return reply with  banner id (\\d+)", (String id) -> {
+	    try {
+		messageReply = returnQueue.take();
+	    } catch (InterruptedException e) {
+		com.undertone.automation.assertion.Assert.fail("interrupted", e);
+	    }
+	    Assert.assertTrue(messageReply.contains(id));
+	});
     }
-    public void init(){
-        //the consumer will wait on this queue
-        returnQueue = new LinkedBlockingDeque<>();
-
-        // will init all the values from the config map object
-        ConnectionFactory fact = new ConnectionFactory();
-        fact.setUsername(config.get("de.rabbitmq.user"));
-        fact.setPassword(config.get("de.rabbitmq.password"));
-        fact.setHost(config.get("de.rabbitmq.host"));
-        fact.setPort(Integer.parseInt(config.get("de.rabbitmq.port")));
-
-//        fact.setUsername("noam");
-//        fact.setPassword("noam");
-//        fact.setHost("172.31.48.21");
-//        fact.setPort(5672);
-        try {
-            conn = fact.newConnection();
-            //consumer_tag is basically thread num
-            publisher = new RabbitMQPublisher("in",conn.createChannel());
-            consumer = new RabbitMQConsumer(this, conn.createChannel(),"test",
-                    "out", "integration-test",
-                    "integration-test");
-            consumer.start();
-        } catch (IOException | TimeoutException  e) {
-            logger.error(e.getMessage());
-        }
-
-    }
-
 
     @Override
     public void processMsg(String msg, String routingKey) {
-        try {
-            returnQueue.put(msg);
-        } catch (InterruptedException e) {
-            
-            logger.error("Got exception ", e);
-        }
+	try {
+	    returnQueue.put(msg);
+	} catch (InterruptedException e) {
+
+	    logger.error("Got exception ", e);
+	}
     }
 }
-
