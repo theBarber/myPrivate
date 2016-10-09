a609c0e99dc363c5497765bc6ed396f874795061
Aharon Hacmon <aharon.hacmon@perion.com>
2016-10-09 15:15:38 +0300

    UT-1440 Write automated tests for the healthcheck functionality in Java
    All healthcheck tests are tagged as sanity
    
    Task-Url: https://jira.perion.com/browse/UT-1440

--- src/test/java/com/undertone/ramp/lift/adselector/automation/UASHealthCheckTest.java
+++ src/test/java/com/undertone/ramp/lift/adselector/automation/UASHealthCheckTest.java
@@ -1,7 +1,11 @@
 package com.undertone.ramp.lift.adselector.automation;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
@@ -31,15 +35,20 @@ public class UASHealthCheckTest extends BaseTest {
     }
 
     public void allResponsesHaveCode(Integer expectedResponseCode) {
-	uas.get().responses().map(f -> f.thenApply(response -> response.getStatusLine().getStatusCode())
-		.whenComplete((statuscode, failure) -> {
-		    if (failure != null) {
-			com.undertone.automation.assertion.Assert.fail("unable to get response", failure);
-			return;
-		    }
-		    Assert.assertThat(statuscode, Matchers.is(expectedResponseCode));
-
-		})).forEach(CompletableFuture::join);
+	uas.get().responses().map(f -> f.thenApply(HttpResponse::getStatusLine).thenApply(StatusLine::getStatusCode)
+		.whenComplete(assertThatResponseCodeIs(expectedResponseCode))).forEach(CompletableFuture::join);
     }
-
+ 
+    private BiConsumer<Integer, Throwable> assertThatResponseCodeIs(int expected) {
+	return (statuscode, failure) -> {
+	    if (failure != null) {
+		com.undertone.automation.assertion.Assert.fail("unable to get response", failure);
+		return;
+	    }
+	    Assert.assertThat(statuscode, Matchers.is(expected));
+	};
+    }
+ 
+    
+    
 }
