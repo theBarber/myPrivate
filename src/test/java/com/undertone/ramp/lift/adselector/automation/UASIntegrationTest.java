e077065f1331826d4d852a379226245785000973
Aharon Hacmon <aharon.hacmon@perion.com>
2016-11-13 09:24:09 +0200

    asserting that instead of asserting true

--- src/test/java/com/undertone/ramp/lift/adselector/automation/UASIntegrationTest.java
+++ src/test/java/com/undertone/ramp/lift/adselector/automation/UASIntegrationTest.java
@@ -8,13 +8,16 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isOneOf;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -35,17 +38,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.HttpClients;
-import org.junit.Assert;
+import org.hamcrest.Matchers;
 import org.junit.runner.RunWith;
 
 import com.undertone.automation.cli.process.CliCommandExecution;
-import com.undertone.automation.module.Named;
 import com.undertone.automation.module.WithId;
 import com.undertone.automation.support.StringUtils;
 import com.undertone.qa.Zone;
-import com.undertone.ramp.lift.uas.automation.UASLogModule;
 import com.undertone.ramp.lift.uas.automation.UASRequestModule;
 
+import co.unruly.matchers.OptionalMatchers;
+import co.unruly.matchers.StreamMatchers;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
@@ -56,7 +59,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(features = "classpath:UASIntegration.feature", plugin = { "pretty",
 	"com.undertone.automation.RotatingJSONFormatter:target/cucumber/uas-adselector-integration_$TIMESTAMP$.json" })
 public class UASIntegrationTest extends BaseTest {
-    protected List<UASLogModule> logModules = new ArrayList<>();
+    // protected List<UASLogModule> logModules = new ArrayList<>();
     public final Stream<String> forLogs = Stream.of("clk", "imp");
 
     public UASIntegrationTest() {
@@ -87,13 +90,14 @@ public class UASIntegrationTest extends BaseTest {
 		});
 
 	Then("The responses? has impression-urls?", () -> {
-	    Assert.assertTrue("all of the responses should have a url",
-		    sut.getUASRquestModule().responses().map(UASIntegrationTest::getImpressionUrl)
-			    .map(CompletableFuture::join).allMatch(Optional::isPresent));
+	    assertThat(
+		    "all of the responses should have a url", sut.getUASRquestModule().responses()
+			    .map(UASIntegrationTest::getImpressionUrl).map(CompletableFuture::join),
+		    StreamMatchers.allMatch(is(not(OptionalMatchers.empty()))));
 	});
 
 	Then("The responses? has click-urls?", () -> {
-	    Assert.assertTrue("all of the responses should have a url", sut.getUASRquestModule().responses()
+	    assertTrue("all of the responses should have a url", sut.getUASRquestModule().responses()
 		    .map(UASIntegrationTest::getClickUrl).map(CompletableFuture::join).allMatch(Optional::isPresent));
 	});
 	//
@@ -114,11 +118,11 @@ public class UASIntegrationTest extends BaseTest {
 			urlExtractor = urlExtractor.andThen(f -> f.thenApply(UASIntegrationTest::parsableClickUrl));
 		    }
 
-		    Assert.assertThat(entityType, isOneOf("campaign", "banner", "zone"));
+		    assertThat(entityType, isOneOf("campaign", "banner", "zone"));
 		    Optional<? extends WithId> expectedEntity = sut.getCampaignManager().getterFor(entityType)
 			    .apply(entityName);
-		    Assert.assertTrue("Could not find " + entityType + " named " + entityName,
-			    expectedEntity.isPresent());
+		    assertThat("Could not find " + entityType + " named " + entityName, expectedEntity,
+			    is(not(OptionalMatchers.empty())));
 
 		    Map<String, Long> theAmountOfTheOccurencesOfTheFieldValueById = sut.getUASRquestModule().responses()
 			    .map(urlExtractor).map(CompletableFuture::join).map(UASIntegrationTest::toURL)
@@ -127,42 +131,20 @@ public class UASIntegrationTest extends BaseTest {
 			    .flatMap(entry -> entry.getValue().stream())
 			    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-		    Assert.assertThat(urlType + " urls grouped by " + fieldName,
+		    assertThat(urlType + " urls grouped by " + fieldName,
 			    theAmountOfTheOccurencesOfTheFieldValueById.keySet(), is(not(empty())));
 
 		    long totalResponses = sut.getUASRquestModule().responses().filter(succeededFuture).count();
-		    Assert.assertThat("total responses", totalResponses, greaterThan(10l));
+		    assertThat("total responses", totalResponses, greaterThan(10l));
 
 		    double actualRate = theAmountOfTheOccurencesOfTheFieldValueById
 			    .getOrDefault(expectedEntity.get().getId(), 0L).doubleValue() / totalResponses;
-		    Assert.assertEquals("rate of " + fieldName + " in impression urls", percent.doubleValue(),
+		    assertEquals("rate of " + fieldName + " in impression urls", percent.doubleValue(),
 			    actualRate * 100, 10d);
 
 		});
 	When("I read the latest (clk|imp) log file from uas", (String logType) -> {
-
-	    logModules.stream().filter(Named.nameIs(logType)).forEach(UASLogModule::readLogs);
-	    // String directory = "/var/log/ut-ramp-uas/";
-	    // List<String> allLines = new ArrayList<>();
-	    // this.uasCliConnections.forEach((connectionName, conn) -> {
-	    // try {
-	    // Optional<String> remoteFile = conn.fileList(directory)
-	    // .filter(s -> logType.equals(s.substring(directory.length(),
-	    // logType.length())))
-	    // .sorted(String.CASE_INSENSITIVE_ORDER.reversed()).findFirst();
-	    //
-	    // File tempFile = File.createTempFile("tmp", remoteFile.get());
-	    // conn.get(remoteFile.get(), tempFile);
-	    //
-	    // Files.lines(Paths.get(tempFile.toURI())).collect(Collectors.toCollection(()
-	    // -> allLines));
-	    // tempFile.delete();
-	    // } catch (IOException e) {
-	    // Assert.fail(e.getMessage());
-	    // }
-	    //
-	    // });
-
+	    assertThat(logType + "log file", sut.logFor(logType).readLogs().actual(), is(not(StreamMatchers.empty())));
 	});
 
 	Then("^I filter in the (clk|imp) log to the lines where id at column (\\d+) is the same as in impression-url$",
@@ -172,25 +154,26 @@ public class UASIntegrationTest extends BaseTest {
 			    .map(CompletableFuture::join).map(UASIntegrationTest::toURL).filter(Optional::isPresent)
 			    .map(Optional::get).findFirst().get();
 
-		    String fieldValue = splitQuery(impressionUrl).get("id").get(0);
-		    UASLogModule logModule = logModules.stream().filter(Named.nameIs(logType)).findFirst()
-			    .orElseThrow(IllegalArgumentException::new);
-
-		    List<String> filteredRawData = new ArrayList<>();
-		    logModule.actual().filter(raw -> fieldValue.equals(raw.get(column))).flatMap(List::stream)
-			    .collect(Collectors.toCollection(() -> filteredRawData));
-		    logModule.setActual(filteredRawData);
-
+		    String idFieldValue = splitQuery(impressionUrl).get("id").get(0);
+		    sut.logFor(logType).filter(raw -> idFieldValue.equals(raw.get(column)));
+		    assertThat(sut.logFor(logType).actual(), is(not(StreamMatchers.empty())));
+		});
+	And("The field (\\w+) in the (\\d+) column of the (clk|imp) log is the same as in impression-url",
+		(String fieldName, Integer column ,String logType) -> {
+		    URL impressionUrl = sut.getUASRquestModule().responses().map(UASIntegrationTest::getImpressionUrl)
+			    .map(CompletableFuture::join).map(UASIntegrationTest::toURL).filter(Optional::isPresent)
+			    .map(Optional::get).findFirst().get();
+		    String expectedFieldValue = splitQuery(impressionUrl).get(fieldName).get(0);
+		    assertThat(sut.logFor(logType).actual(), StreamMatchers.allMatch(Matchers.));
+		    
 		});
 
 	When("I want to use cli to execute \\{([^}]+)\\}", (String cmd) -> {
-
 	    sut.uasCliConnections().forEach(conn -> {
 		try {
 		    new CliCommandExecution(conn, cmd).execute();
-
 		} catch (IOException e) {
-		    Assert.fail(e.getMessage());
+		    fail(e.getMessage());
 		}
 	    });
 	});
@@ -213,7 +196,7 @@ public class UASIntegrationTest extends BaseTest {
 
 		    })).map(CompletableFuture::join).map(HttpResponse::getStatusLine).map(StatusLine::getStatusCode)
 		    .forEach(statusCode -> {
-			Assert.assertThat("Status code of impression request", statusCode, is(204));
+			assertThat("Status code of impression request", statusCode, is(204));
 		    });
 	});
 
@@ -221,7 +204,7 @@ public class UASIntegrationTest extends BaseTest {
 	    try {
 		TimeUnit.SECONDS.sleep(seconds);
 	    } catch (InterruptedException e) {
-		Assert.fail(e.getMessage());
+		fail(e.getMessage());
 	    }
 	});
     }
