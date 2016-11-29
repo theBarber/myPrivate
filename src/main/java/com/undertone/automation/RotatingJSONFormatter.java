b3977084e2b0b824ef3dd7863cd2b60e63ccf62f
Aharon Hacmon <aharon.hacmon@perion.com>
2016-11-29 17:56:12 +0200

    fix glue

--- src/main/java/com/undertone/automation/RotatingJSONFormatter.java
+++ src/main/java/com/undertone/automation/RotatingJSONFormatter.java
@@ -6,7 +6,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
-import cucumber.runtime.formatter.CucumberJSONFormatter;;
+import cucumber.runtime.formatter.CucumberJSONFormatter;
+import gherkin.formatter.model.Match;
+import gherkin.formatter.model.Result;;
 
 public class RotatingJSONFormatter extends CucumberJSONFormatter {
     private static final String DOLLAR = Pattern.quote("$TIMESTAMP$");
@@ -22,4 +24,26 @@ public class RotatingJSONFormatter extends CucumberJSONFormatter {
 	f.createNewFile();
 	return new BufferedWriter(new FileWriter(f));
     }
+
+    @Override
+    public void match(Match match) {
+	if (!match.getLocation().contains("LambdaGlueBase.java")) {
+	    super.match(match);
+	}
+    }
+
+    @Override
+    public void before(Match match, Result result) {
+	if (!match.getLocation().contains("LambdaGlueBase.java")) {
+	    super.before(match, result);
+	}
+    }
+
+    @Override
+    public void after(Match match, Result result) {
+	if (!match.getLocation().contains("LambdaGlueBase.java")) {
+	    super.after(match, result);
+	}
+    }
+    
 }
