9896ac44e9e17f2246746061cbb711a2ef1a1203
Hadas.Feldbaum <hadas.feldbaum@perion.com>
2017-07-17 17:14:19 +0300

    add option:zone cache refresh by cmd/http

--- src/test/java/steps/BaseTest.java
+++ src/test/java/steps/BaseTest.java
@@ -129,7 +129,7 @@ public class BaseTest implements En {
     SqlWorkflowUtils.setLimitationForZone(155605, "[]");
     SqlWorkflowUtils.setLimitationForZone(156242, "[]");
     SqlWorkflowUtils.setColumnInWorkflow("campaigns", "campaignname", "ramp-lift-auto-campaign1-test", "units", "-1");
-    CacheProcessTest.refreshZoneCache();
+    CacheProcessTest.refreshZoneCache("http");
     
     try {
       TimeUnit.SECONDS.sleep(140);
--- src/test/java/steps/CacheProcessTest.java
+++ src/test/java/steps/CacheProcessTest.java
@@ -39,7 +39,10 @@ public class CacheProcessTest extends BaseTest {
           SqlWorkflowUtils.setLimitationForZone(zoneId, newLimitation);
         });
     When("^zoneCache refreshed by cmd$", () -> {
-      refreshZoneCache();
+      refreshZoneCache("cmd");
+    });
+    When("^zoneCache refreshed by http$", () -> {
+      refreshZoneCache("http");
     });
     When("limitation for zone (.*) in zoneCache contains \\{([^}]+)\\}",
         (String zoneId, String expectedLimitation) -> {
@@ -69,28 +72,27 @@ public class CacheProcessTest extends BaseTest {
 
   }
 
-  public static void refreshZoneCache() {
-//    boolean isReady = false;
-//    int timeOut = 5000;
-    
-    sut.getUASRquestModule().zoneCacheRequest("refresh");
-//    sut.getUASRquestModule().zoneCacheRequest("query_status");
-    
-//    sut.getUASRquestModule().responses().filter(fh -> HttpContentTest.getContent(fh.join()).contains("ready"));
-    
-//    String cacheZonesCmd = "docker exec ut-ramp-uas adserver --cache zones";
-//
-//    sut.uasCliConnections().forEach(conn -> {
-//      try {
-//        sut.write("Executing " + cacheZonesCmd + " on " + conn.getName() + "["
-//            + Thread.currentThread().getName());
-//        sut.write("********************************************************************");
-//        CliCommandExecution zoneCacheExecution = new CliCommandExecution(conn, cacheZonesCmd)
-//            .error("Couldn't run query").withTimeout(3, TimeUnit.MINUTES);
-//        zoneCacheExecution.execute();
-//      } catch (IOException e) {
-//        throw new UncheckedIOException(e);
-//      }
-//    });
+  public static void refreshZoneCache(String action) {
+    if (action.equals("http")) {
+      sut.getUASRquestModule().zoneCacheRequest("refresh");
+      // sut.getUASRquestModule().zoneCacheRequest("query_status");
+      //
+      // sut.getUASRquestModule().responses().filter(fh -> HttpContentTest.getContent(fh.join()).contains("ready"));
+    } else if (action.equals("cmd")) {
+      String cacheZonesCmd = "docker exec ut-ramp-uas adserver --cache zones";
+      
+      sut.uasCliConnections().forEach(conn -> {
+        try {
+          sut.write("Executing " + cacheZonesCmd + " on " + conn.getName() + "["
+              + Thread.currentThread().getName());
+          sut.write("********************************************************************");
+          CliCommandExecution zoneCacheExecution = new CliCommandExecution(conn, cacheZonesCmd)
+              .error("Couldn't run query").withTimeout(3, TimeUnit.MINUTES);
+          zoneCacheExecution.execute();
+        } catch (IOException e) {
+          throw new UncheckedIOException(e);
+        }
+      });
+    }
   }
 }
