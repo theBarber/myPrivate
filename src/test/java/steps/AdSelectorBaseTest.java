c9a60b176004dfbbc4486b83887a3e15b75f21c1
Hadas.Feldbaum <hadas.feldbaum@perion.com>
2017-07-17 13:55:21 +0300

    change amazon instance

--- src/test/java/steps/AdSelectorBaseTest.java
+++ src/test/java/steps/AdSelectorBaseTest.java
@@ -1,28 +1,12 @@
 package steps;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
-
-import org.junit.Assert;
-import org.junit.runner.RunWith;
-
-import com.amazonaws.services.s3.model.PutObjectResult;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import entities.Zone;
 import infra.utils.HttpContentTest;
-import infra.utils.S3Client;
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
 /**
  * Created by nive on 2016-10-30.
  */
@@ -37,24 +21,24 @@ public class AdSelectorBaseTest extends BaseTest  {
     public AdSelectorBaseTest(){
         super();
 
-        Given("^Loading test_plan (.*) to S3 directory (.*)",(String from, String to)-> {
-            URL url = getClass().getClassLoader().getResource(from);
-            PutObjectResult res = (new S3Client()).uploadFile(getClass().getClassLoader().getResource(from).getPath(), to);
-
-            // TODO: 2016-10-31 add regex with timestamp to slice id
-            try {
-                File tmp = File.createTempFile("tempTest1", ".tmp");
-                Files.copy(Paths.get(url.toURI()),tmp.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                String tmpSlice = slice.replace("#sliceId#", String.valueOf(System.currentTimeMillis()));
-                sut.write(tmpSlice);
-                Files.write(Paths.get(tmp.toURI()), tmpSlice.getBytes(), StandardOpenOption.APPEND);
-
-                tmp.delete();
-                assertThat(res, is(notNullValue()));
-            } catch (IOException | URISyntaxException e) {
-                Assert.assertNull(e.getMessage(),e);
-            }
-        });
+//        Given("^Loading test_plan (.*) to S3 directory (.*)",(String from, String to)-> {
+//            URL url = getClass().getClassLoader().getResource(from);
+//            PutObjectResult res = (new S3Client()).uploadFile(getClass().getClassLoader().getResource(from).getPath(), to);
+//
+//            // TODO: 2016-10-31 add regex with timestamp to slice id
+//            try {
+//                File tmp = File.createTempFile("tempTest1", ".tmp");
+//                Files.copy(Paths.get(url.toURI()),tmp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+//                String tmpSlice = slice.replace("#sliceId#", String.valueOf(System.currentTimeMillis()));
+//                sut.write(tmpSlice);
+//                Files.write(Paths.get(tmp.toURI()), tmpSlice.getBytes(), StandardOpenOption.APPEND);
+//
+//                tmp.delete();
+//                assertThat(res, is(notNullValue()));
+//            } catch (IOException | URISyntaxException e) {
+//                Assert.assertNull(e.getMessage(),e);
+//            }
+//        });
         Then("^The banner to passback ratio for banner \\{(.+)\\} should be (\\d+)%", (String bannerId, Integer percentage) -> {
             long total  = sut.getUASRquestModule().responses().count();
             long numOfImp = sut.getUASRquestModule().responses().filter(fh -> HttpContentTest.getContent(fh.join()).contains("/l?bannerid=")).count();
