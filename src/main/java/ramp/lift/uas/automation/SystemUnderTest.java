d8a5e2fe48c660ce7c9648243da8b8d8e3ac0b1b
Aharon Hacmon <aharon.hacmon@perion.com>
2017-07-25 16:47:06 +0300

    configuration host->domain

--- src/main/java/ramp/lift/uas/automation/SystemUnderTest.java
+++ src/main/java/ramp/lift/uas/automation/SystemUnderTest.java
@@ -80,7 +80,7 @@ public class SystemUnderTest extends AbstractModuleImpl<SystemUnderTest> impleme
 				if (uas == null) {
 					try {
 						uas = new UASRequestModule();
-						uas.setHost(config.get("uas.host"));
+						uas.setDomain(config.get("uas.domain"));
 						uas.setPort(config.get("uas.port"));
 						uas.init();
 					} catch (Exception e) {
@@ -313,7 +313,7 @@ public class SystemUnderTest extends AbstractModuleImpl<SystemUnderTest> impleme
 	  if (uas == null) {
         try {
             uas = new UASRequestModule();
-            uas.setHost(config.get("uas.host"));
+            uas.setDomain(config.get("uas.host"));
             uas.setPort(config.get("uas.port"));
             uas.init();
         } catch (Exception e) {
