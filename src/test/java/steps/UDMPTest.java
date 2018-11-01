0f1bd83a3c3f4869ed00f9e64077b7692b4a2b4a
Yanivc <yanivc@perion.com>
2018-11-01 11:46:52 +0200

    add parallel support

--- src/test/java/steps/UDMPTest.java
+++ src/test/java/steps/UDMPTest.java
@@ -4,14 +4,14 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-public class UDMPTest {
-    @RunWith(Cucumber.class)
-    @CucumberOptions(features = "classpath:UDMP.feature", plugin = {"pretty",
-            "infra.RotatingJSONFormatter:target/cucumber/UDMP_$TIMESTAMP$.json"})
-    public class UDMP extends BaseTest{
-        public UDMP(){
-            super();
-        }
-    }
 
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:UDMP.feature", plugin = {"pretty",
+        "infra.RotatingJSONFormatter:target/cucumber/UDMP_$TIMESTAMP$.json"})
+public class UDMPTest extends BaseTest{
+    public UDMPTest(){
+        super();
+    }
 }
+
+
