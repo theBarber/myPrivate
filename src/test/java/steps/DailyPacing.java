458b445fe84d4494470c2a35f7052646964b84c8
daniellad <daniellad@perion.com>
2018-12-03 14:20:22 +0200

    DT fixed

---
+++ src/test/java/steps/DailyPacing.java
@@ -0,0 +1,11 @@
+package steps;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:DailyPacing.feature", plugin = {"pretty",
+        "infra.RotatingJSONFormatter:target/cucumber/DailyPacing_$TIMESTAMP$.json"})
+public class DailyPacing {
+}
