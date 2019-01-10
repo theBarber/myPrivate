c4e9b4617e667e74e357862518c966c271645502
daniellad <daniellad@perion.com>
2019-01-10 11:08:32 +0200

    DT fixed

---
+++ src/test/java/steps/DynamicHeaderBidingBidPriceTest.java
@@ -0,0 +1,11 @@
+package steps;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:DynamicHeaderBidingBidPrice.feature", plugin = {"pretty",
+        "infra.RotatingJSONFormatter:target/cucumber/DynamicHeaderBidingBidPrice_$TIMESTAMP$.json"})
+public class DynamicHeaderBidingBidPriceTest {
+}
