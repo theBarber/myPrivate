f6f81d333ba0f2f62eb37547df2033554b15b208
Doron Smoliansky <dorons@undertone.com>
2019-09-03 17:32:24 +0300

    change athena workbench

--- src/main/java/infra/utils/AthenaClient/ExampleConstants.java
+++ src/main/java/infra/utils/AthenaClient/ExampleConstants.java
@@ -3,9 +3,9 @@ package infra.utils.AthenaClient;
 public class ExampleConstants {
 
     public static final int CLIENT_EXECUTION_TIMEOUT = 100000;
-    public static final String ATHENA_OUTPUT_BUCKET = "s3://dl-data-assets-stg";
+    public static final String ATHENA_OUTPUT_BUCKET = "s3://undertone-staging-data";
     // This is querying a table created by the getting started tutorial in Athena
-    public static final String ATHENA_SAMPLE_QUERY = "select * from dl_data_assets.fact_dam_ssp where dt = '20190831' limit 10;";
+    public static final String ATHENA_SAMPLE_QUERY = "select * from dl_raw_data_stg.fact_uas_requests limit 10;";
     public static final long SLEEP_AMOUNT_IN_MS = 1000;
     public static final String ATHENA_DEFAULT_DATABASE = "default";
 
