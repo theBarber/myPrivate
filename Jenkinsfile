cd75eb113fafcf6db09d8c5b3ecc29ba20eef7c6
doronsm <49147616+doronsm@users.noreply.github.com>
2019-12-31 18:55:59 +0200

    S3client acl (#198)
    tweaks in jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -11,6 +11,20 @@ pipeline {
                 }
             }
         }
+//        stage('Restart Mock Server'){
+//            steps{
+//                dir('perion-automation'){
+//                    sh '''#!/bin/bash
+//                        sudo ssh -o StrictHostKeyChecking=no -t -i pems/undertone-dev.pem ubuntu@auto-mock.ramp-ut.io "cd ./mock_server/codefuel-sdmg-mock; pwd; sudo git pull https://'codefuel-automation':'codefuel-automation1$'@github.com/PerionNet/codefuel-sdmg-mock.git master"
+//                        sudo ssh -o StrictHostKeyChecking=no -t -i pems/undertone-dev.pem ubuntu@auto-mock.ramp-ut.io "sudo service mockserver restart"
+//                        sudo ssh -o StrictHostKeyChecking=no -t -i pems/undertone-dev.pem ubuntu@auto-mock.ramp-ut.io "sudo service mockserver1 restart"
+//                        sudo ssh -o StrictHostKeyChecking=no -t -i pems/undertone-dev.pem ubuntu@auto-mock.ramp-ut.io "sudo service mockserver2 restart"
+//                        sudo ssh -o StrictHostKeyChecking=no -t -i pems/undertone-dev.pem ubuntu@auto-mock.ramp-ut.io "sudo service mockserver3 restart"
+//                        sudo ssh -o StrictHostKeyChecking=no -t -i pems/undertone-dev.pem ubuntu@auto-mock.ramp-ut.io "sudo service hubmockserver restart"
+//                    '''
+//                }
+//            }
+//        }
         stage('Create Entities') {
             steps {
                 script {
@@ -19,11 +33,12 @@ pipeline {
                         if [ ${CREATE_ENTITIES} = 'true' ]; then
                         docker build -t ${ENVIRONMENT}-entities-${BUILD_NUMBER} . --build-arg TAGS_TO_RUN=@preconditions --build-arg ENVIRONMENT=${ENVIRONMENT}
                         docker create --name temporary-container-${ENVIRONMENT}-entities-${BUILD_NUMBER} ${ENVIRONMENT}-entities-${BUILD_NUMBER}
-                        docker cp temporary-container-${ENVIRONMENT}-entities-${BUILD_NUMBER}:/target/ ./entities/
+                        docker cp temporary-container-${ENVIRONMENT}-entities-${BUILD_NUMBER}:/ramp-lift-automation/target/ ./entities/
                         docker rm temporary-container-${ENVIRONMENT}-entities-${BUILD_NUMBER}
                         docker rmi ${ENVIRONMENT}-entities-${BUILD_NUMBER}
                         fi'''
                     } catch (exc) {
+                        currentBuild.currentResult = 'FAILURE'
                         throw new Exception("Error creating entities!")
                     }
                 }
@@ -31,24 +46,29 @@ pipeline {
         }
         stage('Test') {
             steps {
-                sh 'pwd'
-                sh 'ls -la'
-                sh 'docker build -t ${ENVIRONMENT}-suite-${BUILD_NUMBER} . --build-arg TAGS_TO_RUN=${TAGS_TO_RUN} --build-arg SUITE_NAME=${SUITE_NAME} --build-arg ENVIRONMENT=${ENVIRONMENT}'
-                sh 'docker create --name temporary-container-${ENVIRONMENT}-suite-${BUILD_NUMBER} ${ENVIRONMENT}-suite-${BUILD_NUMBER}'
-                sh 'docker cp temporary-container-${ENVIRONMENT}-suite-${BUILD_NUMBER}:/target/ ./target/'
-                sh '''#!/bin/bash
-                    if [ ${CREATE_ENTITIES} = 'true' ]; then
-                    rsync -a ./entities/ ./target/
-                    fi'''
-                sh 'docker rm temporary-container-${ENVIRONMENT}-suite-${BUILD_NUMBER}'
-                sh 'docker rmi ${ENVIRONMENT}-suite-${BUILD_NUMBER}'
+                script {
+                    try {
+                        sh 'docker build -t ${ENVIRONMENT}-suite-${BUILD_NUMBER} . --build-arg TAGS_TO_RUN=${TAGS_TO_RUN} --build-arg SUITE_NAME=${SUITE_NAME} --build-arg ENVIRONMENT=${ENVIRONMENT}'
+                        sh 'docker create --name temporary-container-${ENVIRONMENT}-suite-${BUILD_NUMBER} ${ENVIRONMENT}-suite-${BUILD_NUMBER}'
+                        sh 'docker cp temporary-container-${ENVIRONMENT}-suite-${BUILD_NUMBER}:/ramp-lift-automation/target/ ./target/'
+                        sh '''#!/bin/bash
+                            if [ ${CREATE_ENTITIES} = 'true' ]; then
+                            rsync -a ./entities/ ./target/
+                            fi'''
+                        sh 'docker rm temporary-container-${ENVIRONMENT}-suite-${BUILD_NUMBER}'
+                        sh 'docker rmi ${ENVIRONMENT}-suite-${BUILD_NUMBER}'
+                    } catch (exc) {
+                        currentBuild.currentResult = 'FAILURE'
+                        throw new Exception(exc)
+                    }
+                }
             }
         }
         stage('Publish') {
             steps {
                 sh '''#!/bin/bash
                     if [ ${JIRA_REPORT} = 'true' ]; then
-                    for f in ./ramp-ui-automation/target/surefire-reports/junitreports/*.xml; do
+                    for f in ./target/surefire-reports/*.xml; do
                     curl -H "Content-Type: multipart/form-data" -u jiraservice:!qaz2wsX -F "file=@$f" "https://dev.perion.com/rest/raven/1.0/import/execution/junit?projectKey=Undertone&testExecKey=${TEST_EXEC_KEY}&revision=v1.0.0&testEnvironments=${ENVIRONMENT}"
                     done
                     fi'''
@@ -58,7 +78,6 @@ pipeline {
     post {
         always {
             junit 'target/surefire-reports/*.xml'
-            cleanWs()
         }
         changed {
             script {
@@ -75,6 +94,9 @@ pipeline {
                             to: '$DEFAULT_RECIPIENTS'
                 }
             }
+        }
+        cleanup {
+            cleanWs()
         }
     }
 }
--- src/main/java/infra/utils/S3Client.java
+++ src/main/java/infra/utils/S3Client.java
@@ -1,6 +1,8 @@
 package infra.utils;
 
 import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -24,88 +26,110 @@ public class S3Client {
     private static AmazonS3 amazonS3;
 
     public S3Client() {
-	Properties prop = new Properties();
-	try {
-	    BufferedReader in = new BufferedReader(new FileReader(PROP_PATH));
-	    prop.load(in);
-	    in.close();
-	} catch (IOException e) {
-	    Assert.fail(e.getMessage());
-	}
-	AWSCredentials s3Credential = new BasicAWSCredentials(prop.getProperty("s3_access_key"),
-		prop.getProperty("s3_secret_key"));
-	
-	AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(s3Credential);
-	amazonS3 = AmazonS3ClientBuilder.standard().withRegion(prop.getProperty("s3_region")).withCredentials(credentialsProvider).build();
+        Properties prop = new Properties();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(PROP_PATH));
+            prop.load(in);
+            in.close();
+        } catch (IOException e) {
+            Assert.fail(e.getMessage());
+        }
+        try {
+        AWSCredentials s3Credential = new BasicAWSCredentials(prop.getProperty("s3_access_key"),
+                prop.getProperty("s3_secret_key"));
 
+        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(s3Credential);
+        amazonS3 = AmazonS3ClientBuilder
+                .standard()
+                .withRegion(prop.getProperty("s3_region"))
+                .withCredentials(credentialsProvider)
+                .build();
+        // Get the existing object ACL that we want to modify.
+        AccessControlList acl = amazonS3.getObjectAcl("ramp-delievery-qa", prop.getProperty("s3_secret_key"));
+
+        // Clear the existing list of grants.
+        acl.getGrantsAsList().clear();
+
+        // Grant a sample set of permissions, using the existing ACL owner for Full Control permissions.
+//        acl.grantPermission(new CanonicalGrantee(acl.getOwner().getId()), Permission.FullControl);
+//        acl.grantPermission(new EmailAddressGrantee(emailGrantee), Permission.WriteAcp);
+
+        // Save the modified ACL back to the object.
+        amazonS3.setObjectAcl("ramp-delievery-qa", prop.getProperty("s3_secret_key"), acl);
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it, so it returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
     }
 
     public PutObjectResult uploadFile(String from, String to) {
-	PutObjectResult res = null;
-	try {
-	    int bucketDel = to.indexOf('/');
-	    File file = new File(from);
-	    String uploadKey = to.substring(bucketDel + 1);
-	    res = amazonS3.putObject(new PutObjectRequest(to.substring(0, bucketDel), uploadKey, file));
-	} catch (AmazonClientException e) {
-	    Assert.fail(e.getMessage());
-	}
-	return res;
+        PutObjectResult res = null;
+        try {
+            int bucketDel = to.indexOf('/');
+            File file = new File(from);
+            String uploadKey = to.substring(bucketDel + 1);
+            res = amazonS3.putObject(new PutObjectRequest(to.substring(0, bucketDel), uploadKey, file));
+        } catch (AmazonClientException e) {
+            Assert.fail(e.getMessage());
+        }
+        return res;
     }
 
     public ObjectMetadata downloadFile(String from, String to) {
-	ObjectMetadata res = null;
-	try {
-	    int bucketDel = from.indexOf('/');
-	    File file = new File(to);
-	    String uploadKey = from.substring(bucketDel + 1);
-	    res = amazonS3.getObject(new GetObjectRequest(from.substring(0, bucketDel), uploadKey), file);
-	} catch (AmazonClientException e) {
-	    Assert.fail(e.getMessage());
-	}
-	return res;
+        ObjectMetadata res = null;
+        try {
+            int bucketDel = from.indexOf('/');
+            File file = new File(to);
+            String uploadKey = from.substring(bucketDel + 1);
+            res = amazonS3.getObject(new GetObjectRequest(from.substring(0, bucketDel), uploadKey), file);
+        } catch (AmazonClientException e) {
+            Assert.fail(e.getMessage());
+        }
+        return res;
     }
 
     public void deleteFile(String f) {
-	try {
-	    int bucketDel = f.indexOf('/');
-	    amazonS3.deleteObject(f.substring(0, bucketDel), f.substring(bucketDel + 1));
-	} catch (AmazonClientException e) {
-	    Assert.fail(e.getMessage());
-	}
+        try {
+            int bucketDel = f.indexOf('/');
+            amazonS3.deleteObject(f.substring(0, bucketDel), f.substring(bucketDel + 1));
+        } catch (AmazonClientException e) {
+            Assert.fail(e.getMessage());
+        }
     }
 
-	public void assertFileContains(String fileName, String value) {
-		S3Object res = null;
-		try {
-			int bucketDel = fileName.indexOf('/');
-			String uploadKey = fileName.substring(bucketDel + 1);
-			res = amazonS3.getObject(new GetObjectRequest(fileName.substring(0, bucketDel), uploadKey));
-		} catch (AmazonClientException e) {
-			Assert.fail(e.getMessage());
-		}
-		Assert.assertThat("The s3 file doesn't contains the value of: "+value,isFileContentContains(res.getObjectContent(),value),is(true));
-	}
+    public void assertFileContains(String fileName, String value) {
+        S3Object res = null;
+        try {
+            int bucketDel = fileName.indexOf('/');
+            String uploadKey = fileName.substring(bucketDel + 1);
+            res = amazonS3.getObject(new GetObjectRequest(fileName.substring(0, bucketDel), uploadKey));
+        } catch (AmazonClientException e) {
+            Assert.fail(e.getMessage());
+        }
+        Assert.assertThat("The s3 file doesn't contains the value of: " + value, isFileContentContains(res.getObjectContent(), value), is(true));
+    }
 
-	private boolean isFileContentContains(InputStream inputStreamContent, String value)
-	{
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStreamContent));
-		try{
-			while (reader.ready()) {
-				String line = reader.readLine();
-				if(line!=null) // maybe adding check that smaller than 200 character not valid..
-				{
-					if(line.contains(value))
-						return true;
-				}else
-					{
-						break;
-					}
-			}
-		}catch (IOException e)
-		{
-			Assert.fail(e.getMessage());
-		}
-	return false;
-	}
+    private boolean isFileContentContains(InputStream inputStreamContent, String value) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStreamContent));
+        try {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                if (line != null) // maybe adding check that smaller than 200 character not valid..
+                {
+                    if (line.contains(value))
+                        return true;
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            Assert.fail(e.getMessage());
+        }
+        return false;
+    }
 }
