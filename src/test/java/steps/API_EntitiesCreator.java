9a2d94a9ffecfeb8740506e648e108d11f9cf036
Eitan Shlomi <eitans@perion.com>
2020-04-06 08:57:44 +0300

    fixing update campaign day pacing

--- src/test/java/steps/API_EntitiesCreator.java
+++ src/test/java/steps/API_EntitiesCreator.java
@@ -488,18 +488,16 @@ public class API_EntitiesCreator extends BaseTest {
     }
 
     private void updateCampaignEndDate(String campaign_name, Integer days) {
-
+        int actualDays = days-1;
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         TimeZone etTimeZone = TimeZone.getTimeZone("America/New_York");
         formatter.setTimeZone(etTimeZone);
-
         Date date = new Date();
         String currentDate = formatter.format(date);
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         cal.add(Calendar.DATE, days);
         String endDate = formatter.format(cal.getTime());
-
         SqlWorkflowUtils.WorkflowQuery("UPDATE `undertone`.`campaigns` SET `expire` = '" + endDate + "', `activate` = '" + currentDate + "' WHERE `campaignname` like '%" + campaign_name + "%' and `status` = 0;");
     }
 
