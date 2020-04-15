cb9eb59b56c430dbccf2fa11f245a0b4073e82fe
Eitan Shlomi <eitans@perion.com>
2020-04-15 17:59:58 +0300

    changing 100ms time between requests in throttling scenario

--- src/main/java/ramp/lift/uas/automation/UASRequestModule.java
+++ src/main/java/ramp/lift/uas/automation/UASRequestModule.java
@@ -506,7 +506,7 @@ public class UASRequestModule extends AbstractModuleImpl<List<CompletableFuture<
                 sut.getUASRquestModule().clearCookies();
             synchronizedResponses.add(postRequest(url, body));
             try {
-                TimeUnit.MILLISECONDS.sleep(200);
+                TimeUnit.MILLISECONDS.sleep(100);
             } catch (InterruptedException e) {
                 fail(e.getMessage());
             }
