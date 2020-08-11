d248c64eb8ebb2d5d11c4787e3ea7f4e7334f0d7
Eitan Shlomi <eitans@perion.com>
2020-08-11 07:51:16 +0300

    fixing floor price

--- src/test/java/steps/HeaderBiddingMinFloorPriceTest.java
+++ src/test/java/steps/HeaderBiddingMinFloorPriceTest.java
@@ -5,8 +5,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:HeaderBiddingMinFloorPrice.feature", plugin = { "pretty"})
+@RunWith(Cucumber.class)
 public class HeaderBiddingMinFloorPriceTest extends  BaseTest {
     public HeaderBiddingMinFloorPriceTest() {
     }
